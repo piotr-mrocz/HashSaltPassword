3964bc3937a84c851901fc90b0861bd9c33d4cb8
piotr-mrocz <p.mrocz@vp.pl>
2022-04-05 20:43:05 +0200

    Try to learn how to hash and salt password

---
+++ HashSaltPassword/HashSaltPassword/HashSaltPassword.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashSaltPassword
+{
+    public static class HashSaltPassword
+    {
+        public static void GetHasPassword()
+        {
+            Console.Write("Wprowadź hasło: ");
+            var password = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(password))
+            {
+                Console.WriteLine("Nie wprowadzono hasła!");
+                return;
+            }
+
+            var hashPassword = BCrypt.Net.BCrypt.HashPassword(password);
+            Console.WriteLine("HashPassword: " + hashPassword);
+            Console.WriteLine();
+
+            var salt = BCrypt.Net.BCrypt.GenerateSalt();
+            Console.WriteLine("Salt: " + salt);
+            Console.WriteLine();
+
+            var saltPassword = BCrypt.Net.BCrypt.HashPassword(password, salt);
+            Console.WriteLine("SaltPassword: " + saltPassword);
+            Console.WriteLine();
+
+
+            var saltPasswordEnhanced = BCrypt.Net.BCrypt.HashPassword(password, salt, true);
+            Console.WriteLine("SaltPasswordEnhanced: " + saltPasswordEnhanced);
+            Console.WriteLine();
+
+            Console.Write("Wprowadź hasło do sprawdzenia uproszczonego: ");
+            var passwordToVeryfy = Console.ReadLine();
+
+            var veryfiedPassword = BCrypt.Net.BCrypt.Verify(passwordToVeryfy, saltPassword, false);
+            Console.WriteLine("Wynik weryfikacji: " + veryfiedPassword);
+            Console.WriteLine();
+
+            Console.Write("Wprowadź hasło do sprawdzenia ulepszonego: ");
+            var passwordToVeryfyEnhanced = Console.ReadLine();
+
+            var passwordToVeryfyEnhancedHashed = BCrypt.Net.BCrypt.HashPassword(passwordToVeryfyEnhanced, BCrypt.Net.BCrypt.GenerateSalt(), true);
+            Console.WriteLine("passwordToVeryfyEnhancedHashed: " + passwordToVeryfyEnhancedHashed);
+            Console.WriteLine();
+
+            var veryfiedPasswordEnhanced = BCrypt.Net.BCrypt.Verify(passwordToVeryfyEnhanced, passwordToVeryfyEnhancedHashed, true);
+            Console.WriteLine("Wynik weryfikacji: " + veryfiedPasswordEnhanced);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ HashSaltPassword/HashSaltPassword/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Person 1");
+Console.WriteLine();
+HashSaltPassword.HashSaltPassword.GetHasPassword();
+
+Console.WriteLine();
+Console.WriteLine();
+
+Console.WriteLine("Person 2");
+Console.WriteLine();
+HashSaltPassword.HashSaltPassword.GetHasPassword();
